8482f9bc34838449ed5e20fa9b92ee315a21cf69
ajedkrap <mr.thedja@gmail.com>
2020-10-07 00:34:41 +0700

    submit

---
+++ index.js
@@ -0,0 +1,10 @@
+function tugas20(){
+  var string = 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\\\'t look even slightly believable.'
+  var nul = null;
+  match1 = string.match("are");
+  match2 = string.match("words");
+  console.log(match1);
+  console.log(nul);
+  console.log(match2);
+}
+tugas20();
